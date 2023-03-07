301de4274f4eeac2f40c7a5d2c7eb4efabf918a4
Sergey <runix1981@gmail.com>
2023-03-07 14:11:16 +0300

    Add task about 2x massive

---
+++ Task_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Метод создания двухмерного массива
+
+double[,] AddArray(string numrow, string numcol)
+{
+    Random rnd = new Random();
+    System.Console.Write($"{numrow} -> ");
+    int row = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write($"{numcol} -> ");
+    int col = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble()*rnd.Next(-10,11);
+        }
+    }
+    return array;
+
+}
+
+// Метод вывода массива в консоль
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]:f2} | ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(AddArray("Введите количество строк", "Введите количество столбцов"));
